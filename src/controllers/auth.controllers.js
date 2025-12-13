c4ef7529a356721650162ab4c49e118e258faffa
anotherahmadzidan <sumovicz1204@gmail.com>
2025-12-14 02:42:26 +0800

    tambah registrasi user

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcrypt");
+const prisma = require("../config/db");
+
+const register = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email }
+        });
+
+        if (existingUser) {
+            return res.status(409).json({
+                success: false,
+                message: "Email already registered"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            data: {
+                id: user.id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { register };
