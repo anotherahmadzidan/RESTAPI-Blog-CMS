d1b73529a8a29aeab84868673860d962c885d089
anotherahmadzidan <sumovicz1204@gmail.com>
2025-12-17 16:49:45 +0800

    fix pilih tag di post

--- src/validators/post.validator.js
+++ src/validators/post.validator.js
@@ -3,14 +3,16 @@ const { z } = require("zod");
 const createPostSchema = z.object({
     title: z.string().min(3, "Title must be at least 3 characters"),
     content: z.string().min(10, "Content must be at least 10 characters"),
-    categoryId: z.number().int().positive()
+    categoryId: z.number().int().positive(),
+    tags: z.array(z.number()).optional()
 });
 
 const updatePostSchema = z
     .object({
         title: z.string().min(3).optional(),
         content: z.string().min(10).optional(),
-        categoryId: z.number().int().positive().optional()
+        categoryId: z.number().int().positive().optional(),
+        tags: z.array(z.number()).optional()
     })
     .refine(
         (data) => Object.keys(data).length > 0,
